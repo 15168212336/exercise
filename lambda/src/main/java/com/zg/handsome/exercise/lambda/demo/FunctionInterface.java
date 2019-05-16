32d9c9a2d5ff467a29ee03691b49d342a3f3bf29
zgyy <695466133@qq.com>
2019-05-16 17:04:16 +0800

    lambda练习

---
+++ lambda/src/main/java/com/zg/handsome/exercise/lambda/demo/FunctionInterface.java
@@ -0,0 +1,34 @@
+package com.zg.handsome.exercise.lambda.demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FunctionInterface {
+
+    @FunctionalInterface
+    public interface Checker<T extends String> {
+        Boolean check(T t);
+    }
+
+    @FunctionalInterface
+    interface Out<T>{
+        void achievement(T t);
+    }
+
+    @Test
+    public void testLambda() {
+        List<String> demoList = Arrays.asList("格瑞", "Mr.ming", "金");
+        rollCall(demoList,checker -> checker.startsWith("z"),out -> System.out.println(out));
+    }
+
+    public void rollCall(List<? extends String> list, Checker checker,Out out){
+        for(String name : list){
+            if(checker.check(name)){
+                out.achievement(name);
+            }
+        }
+    }
+
+}
